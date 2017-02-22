1f3fdcea0b99790392740efe3a0e01c728319d07
joshion <joshion@outlook.com>
2017-02-22 09:20:09 +0800

    mv some files to another floder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var Student = require("./db_schema").Student_Model;
-var users_router = require("./users");
+var Student = require("./lib/db_schema").Student_Model;
+var users_router = require("./lib/users");
 var app = express();
 
 var api_router = express.Router();
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 app.use('/api', api_router);
 api_router.use('/users', users_router);
 
-app.listen(1337, function(){
+app.listen(80, function(){
     console.log("Server listen on 1337..."); 
 });
 
